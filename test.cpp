00338b515ec50cc62d9c6685f517e9ba86bafd6b
Roman K <kromchyk@yahoo.com>
2022-06-03 12:16:40 +0300

    CPP files

---
+++ test.cpp
@@ -0,0 +1,60 @@
+#include <iostream>       
+#include <thread>         
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+static int found=0, start=0;
+static string initpoint="C:\\";
+
+string tolowerstr(string data)
+{
+  std::for_each(data.begin(),data.end(),[](char & c) {
+  c = std::tolower(c);
+  return data;
+}
+ 
+void file_scan(string filename) 
+{
+filename=tolowerstr(filename);
+while(1) {
+	if !start  {
+	start=1;	
+    for(auto& p: std::filesystem::recursive_directory_iterator(initpoint)) {
+    if(found) return;
+    if p.depth()==1 {
+	  std::filesystem::recursive_directory_iterator& t=p;
+	  initpoint=(t++).path();
+	  start=0;
+	}
+      if p.is_regular_file&&tolowerstr(p.path).find(filename)
+	  {
+       found=1; 
+	   cout << "Found file : " << filename << "\n";
+	   cout << "Full file path : " << p.path;
+	   string fileout = "Test.b";
+       std::ofstream ostrm(fileout, std::ios::out);
+       ostrm << "Found file : " << filename << "\n";
+	   ostrm << "Full file path : " << p.path;
+	   ostrm.close();
+	 }
+	}
+	}
+}
+}
+int main() 
+{
+  std::thread threads[8];                         
+  try
+   {
+  for (int i=0; i<8; i++) {
+    threads[i] = std::thread(file_scan,"name_file.txt");
+    if(threads[i].joinable())
+    threads[i].detach(); }
+   } catch (exception& e)
+     {
+       cout << e.what() << '\n';
+     }
+  
+return 0;
+}
